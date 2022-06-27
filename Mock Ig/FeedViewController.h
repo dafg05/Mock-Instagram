e90cbde81f094bec95b0183d1f40621e69615aa0
dafg05 <danialefloresg@gmail.com>
2022-06-27 13:59:45 -0700

    Add skeleton view controllers

---
+++ Mock Ig/FeedViewController.h
@@ -0,0 +1,16 @@
+//
+//  FeedViewController.h
+//  Mock Ig
+//
+//  Created by Daniel Flores Garcia on 6/27/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface FeedViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
