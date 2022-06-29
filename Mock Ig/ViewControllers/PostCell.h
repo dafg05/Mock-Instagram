2c2829071d1cc10d7156e29301b8855effdd3037
dafg05 <danialefloresg@gmail.com>
2022-06-29 15:08:45 -0700

    Feed and details view

---
+++ Mock Ig/ViewControllers/PostCell.h
@@ -0,0 +1,16 @@
+//
+//  PostCell.h
+//  Mock Ig
+//
+//  Created by Daniel Flores Garcia on 6/29/22.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface PostCell : UITableViewCell
+
+@end
+
+NS_ASSUME_NONNULL_END
